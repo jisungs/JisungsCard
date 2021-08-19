1f05c9bd233a688c874c5ecbac6c8dc4889f8bab
jisung <jisung.smart@gmail.com>
2021-08-19 14:42:34 +0900

    Initial Commit

---
+++ JisungsCard/JisungsCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JisungsCardApp.swift
+//  JisungsCard
+//
+//  Created by jisung on 2021/08/19.
+//
+
+import SwiftUI
+
+@main
+struct JisungsCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
